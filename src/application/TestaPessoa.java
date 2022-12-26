1749ea6ddea8a4e009d65f431c3d6dea1bcddd81
Ian Vasconcelos <99202627+ianvscn@users.noreply.github.com>
2022-12-26 15:22:17 -0300

    Create TestaPessoa.java

---
+++ src/application/TestaPessoa.java
@@ -0,0 +1,80 @@
+package application;
+/*Projeto - Academia Java
+ *		@author: Grupo 2 -
+			Ian Vasconcelos Bernardo
+			Rayanne Augusto
+			Nayara Castro
+			Rebeca Farias
+			
+			
+	CLASSE DE TESTE PARA OS REPOSIT�RIOS VERS�O ARRAY E LISTA
+* */
+
+import model.data.*;
+import model.data.exceptions.InsertException;
+import model.entities.Aluno;
+import model.entities.Curso;
+import model.entities.Pessoa;
+import model.entities.Professor;
+
+public class TestaPessoa {
+	public static void main(String[] args) {
+		
+//	BLOCO DE DECLARA��O E INSTANCIA��O DE OBJETOS TESTE
+		Curso curso1 = new Curso(123, "ADS");
+		Curso curso2 = new Curso(321, "Computa��o");
+
+		Pessoa aluno1 = new Aluno("Nayara", "001", 20, curso1);
+		Pessoa aluno2 = new Aluno("Rebeca", "002", 20, curso2);
+		Pessoa aluno3 = new Aluno("Rayanne", "003", 20, curso1);
+		Pessoa aluno4 = new Aluno("Isa", "004", 20, curso1);
+		Pessoa professor1 = new Professor("Juliana", "005", 26, 8000);
+		Pessoa professor2 = new Professor("Julio", "006", 20, 8000);
+
+		RepositorioPessoa rep = new RepositorioPessoaLista();
+		//RepositorioPessoa rep = new RepositorioPessoaArray(6);
+
+//	BLOCO DE INSER��O E TRATAMENTO	
+		try {
+			rep.inserir(aluno1);
+			rep.inserir(aluno2);
+			rep.inserir(aluno3);
+			rep.inserir(aluno4);
+			rep.inserir(professor1);
+			rep.inserir(professor2);
+		} catch (InsertException e) {
+			System.out.println("");
+			System.out.println(e.getMessage());
+		}
+
+//  BLOCO DE EXIBI��O DE TESTES		
+		System.out.println("\n\n---------- TESTE DE PROCURAR ----------\n");
+		Pessoa alunoProcurado = rep.procurar("003");
+
+		if (alunoProcurado != null)
+			System.out.println("PESSOA PROCURADA(003): " + alunoProcurado);
+		else
+			System.out.println("PESSOA N�O ENCONTRADO!");
+
+		System.out.println("\n\n---------- TESTE DE REMO��O, INSER��O E EXIBI��O ----------");
+
+		System.out.println("\n>>>>> LISTA DE PESSOAS ANTES DA REMO��O <<<<<<");
+		rep.exibir();
+
+		rep.remover("003");
+		rep.remover("005");
+
+		System.out.println("\n\n>>>>> LISTA DE PESSOAS ATUALIZADA <<<<<<");
+		System.out.println("(CPF a ser removido: 005, 003)\n");
+		rep.exibir();
+
+		System.out.println("\n\n---------- PROCURANDO PESSOA APAGADA ----------\n");
+		if (rep.procurar("005") == null)
+			System.out.println("N�O ENCONTRADO! APAGOU!");
+		else
+			System.out.println("ENCONTRADO! N�O APAGOU!");
+
+		System.out.println("\n\n---------- TENTANDO REMOVER PESSOA APAGADA ----------\n");
+		rep.remover("003");
+	}
+}
