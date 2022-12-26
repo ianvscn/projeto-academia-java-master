6530c17f5fa362e7af096eabd6a657328255b1c1
Ian Vasconcelos <99202627+ianvscn@users.noreply.github.com>
2022-12-26 15:25:26 -0300

    Create AlunoDaoContract.java

---
+++ src/model/dao/AlunoDaoContract.java
@@ -0,0 +1,26 @@
+package model.dao;
+/*
+ * INTERFACE AlunoDaoContract
+ * Interface que estabelece os m�todos que devem ser implementados pelo AlunoDao
+ *  
+ * */
+
+import java.util.List;
+
+import model.entities.Aluno;
+
+public interface AlunoDaoContract {
+	
+	boolean inserir(Aluno aluno);
+	
+	boolean atualizar(Aluno aluno);
+	
+	Aluno procurar(String cpf);
+	
+	boolean remover(String cpf);
+	
+	List<Aluno> exibirAlunos();
+	
+	List<Aluno> alunoPorCurso(double codigo);
+	
+}
